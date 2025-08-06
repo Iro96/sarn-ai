14284ab15900704a879ec1b7bed8e050c4bd02c9
Iro <155815508+Iro96@users.noreply.github.com>
2025-08-06 22:36:15 +0700

    Update test_sarn.py

--- tests/test_sarn.py
+++ tests/test_sarn.py
@@ -1,6 +1,10 @@
-from SarnAi.network import SARNNetwork
+from SarnAi import SARNLayer, SARNNetwork
 
 def test_output_shape():
-    model = SARNNetwork(4, 8, 2)
+    input_layer = SARNLayer(input_size=4, output_size=8, k=3)
+    hidden_layer = SARNLayer(input_size=8, output_size=6, k=2)
+    output_layer = SARNLayer(input_size=6, output_size=2, k=1)
+    model = SARNNetwork([input_layer, hidden_layer, output_layer])
     output = model.forward([0.1, 0.2, 0.3, 0.4])
+    model.adapt()
     assert len(output) == 2
